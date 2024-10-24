5eace25b0ff3fa6f5710caa05f7c7866bf68fd09
Yilberth-Arredondo <yiarpe99@gmail.com>
2024-10-24 00:33:52 -0500

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  // Ignorar la carpeta dist
+  { ignores: ['dist'] },
+
+  // Configuración de archivos JS/JSX
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier, // Plugin de Prettier
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': ['error', { singleQuote: true, semi: true }], // Regla de Prettier
+    },
+  },
+
+  // Configuración para Prettier
+  prettierConfig, // Incluir la configuración de eslint-config-prettier para desactivar reglas conflictivas
+];
